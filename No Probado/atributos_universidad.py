e1bea413ee8b38115edaec6bcaa8783e8539d9c1
cascajos <cascajogarcinunojorge@gmail.com>
2026-03-02 19:17:08 +0100

    jorge

--- No Probado/atributos_universidad.py
+++ No Probado/atributos_universidad.py
@@ -1,1 +1,7 @@
-
+class Grados:
+    def __init__(self,nombre,universidad,nota_de_corte,ponderaciones,plazas):
+        self.nombre=nombre
+        self.universidad=universidad
+        self.nota_corte=nota_corte
+        self.ponderaciones=ponderaciones
+        self.plazas=plazas
