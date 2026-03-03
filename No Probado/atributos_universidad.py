3b5f0885904e048c2afb7e31a5cc455dd583fea0
Junfangchong <jianj.fan@educa.jcyl.es>
2026-03-03 12:57:15 +0100

    jun

--- No Probado/atributos_universidad.py
+++ No Probado/atributos_universidad.py
@@ -1,6 +1,5 @@
 class Grados:
-    def __init__(self,nombre,universidad,nota_de_corte,ponderaciones,plazas):
-        self.nombre=nombre
+    def __init__(self,universidad,nota_de_corte,ponderaciones,plazas):
         self.universidad=universidad
         self.nota_corte=nota_de_corte
         self.ponderaciones=ponderaciones
