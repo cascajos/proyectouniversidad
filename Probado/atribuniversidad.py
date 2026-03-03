a40d3a693b310419d9080bb5acf7e3215b0f9b5f
Pauu87 <hermipa22@gmail.com>
2026-03-03 13:26:50 +0100

    paula

---
+++ Probado/atribuniversidad.py
@@ -0,0 +1,23 @@
+class Universidad:
+    def __init__(self, nombre, ciudad, tipo, grados):
+        self.nombre = nombre
+        self.ciudad=ciudad
+        self.tipo=tipo
+        self.grados=grados
+    
+    def main():
+        universidad1 = Universidad(
+            nombre="UBU", ciudad="Burgos", tipo="Pública", grados=["Ingeniería Informática""Derecho""Medicina""Criminología""Farmacéutica""Ingeniería eléctrica""Ingeniería robótica""Física""Matemáticas""Artes""Historia de España""Lengua Castellana""Filosofía"]
+)
+        universidad2 = Universidad(
+            nombre="UPM", ciudad="Madrid", tipo="Pública", grados=["Astronomía""Enfermería""Criminología""Derecho""Geología""Ingeniería Informática""Aviación""Arquitectura""Historia de España""Filosofía""Medicina""Robótica"]
+        )
+        universidad3 =Universidad(
+            nombre="EHU", ciudad="Bilbao", tipo="Pública", grados=["Derecho""Farmacia""Ingeniería Electrónica""Mecánica""Matemáticas""Arte""Biología""Ingeniería Informática""Criminología""Economía"]
+        )
+        universidad4 = Universidad(
+             nombre="UPSAL", ciudad="Salamanca", tipo="Privada", grados=["Física""Medicina""Ingeniería Informática""Matemáticas""Derecho""Robótica""Geología""Filosofía""Criminología""Aviación"]   
+        )
+        universidad5 = Universidad(
+            nombre="UHU", ciudad="Huelva", tipo="Pública", grados=["Ingeniería Mecánica""Farmacia""Biología""Lengua""Filosofía""Ingeniería Informática""Química""Historia de España""Matemáticas""Artes""Psicología""Artes""Física"]
+        )
