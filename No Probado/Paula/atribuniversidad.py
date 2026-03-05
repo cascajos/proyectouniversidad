7c07a62e6fd4a557b4b500210674ccb1756340c4
Pauu87 <hermipa22@gmail.com>
2026-03-05 09:47:41 +0100

    paula

--- No Probado/Paula/atribuniversidad.py
+++ No Probado/Paula/atribuniversidad.py
@@ -7,17 +7,22 @@ class Universidad:
     
     def main():
         universidad1 = Universidad(
-            nombre="UBU", ciudad="Burgos", tipo="Pública", grados=["Ingeniería Informática""Derecho""Medicina""Criminología""Farmacéutica""Ingeniería eléctrica""Ingeniería robótica""Física""Matemáticas""Artes""Historia de España""Lengua Castellana""Filosofía"]
+            nombre="UBU", ciudad="Burgos", tipo="Pública", grados=["Ingeniería Informática""Derecho""Medicina""Criminología""Farmacéutica""Ingeniería eléctrica"
+            "Ingeniería robótica""Física""Matemáticas""Artes""Historia de España""Lengua Castellana""Filosofía"]
         )
         universidad2 = Universidad(
-            nombre="UPM", ciudad="Madrid", tipo="Pública", grados=["Astronomía""Enfermería""Criminología""Derecho""Geología""Ingeniería Informática""Aviación""Arquitectura""Historia de España""Filosofía""Medicina""Robótica"]
+            nombre="UPM", ciudad="Madrid", tipo="Pública", grados=["Astronomía""Enfermería""Criminología""Derecho""Geología""Ingeniería Informática""Aviación"
+            "Arquitectura""Historia de España""Filosofía""Medicina""Robótica"]
         )
         universidad3 =Universidad(
-            nombre="EHU", ciudad="Bilbao", tipo="Pública", grados=["Derecho""Farmacia""Ingeniería Electrónica""Mecánica""Matemáticas""Arte""Biología""Ingeniería Informática""Criminología""Economía"]
+            nombre="EHU", ciudad="Bilbao", tipo="Pública", grados=["Derecho""Farmacia""Ingeniería Electrónica""Mecánica""Matemáticas""Arte""Biología"
+            "Ingeniería Informática""Criminología""Economía"]
         )
         universidad4 = Universidad(
-             nombre="UPSAL", ciudad="Salamanca", tipo="Privada", grados=["Física""Medicina""Ingeniería Informática""Matemáticas""Derecho""Robótica""Geología""Filosofía""Criminología""Aviación"]   
+             nombre="UPSAL", ciudad="Salamanca", tipo="Privada", grados=["Física""Medicina""Ingeniería Informática""Matemáticas""Derecho""Robótica"
+             "Geología""Filosofía""Criminología""Aviación"]   
         )
         universidad5 = Universidad(
-            nombre="UHU", ciudad="Huelva", tipo="Pública", grados=["Ingeniería Mecánica""Farmacia""Biología""Lengua""Filosofía""Ingeniería Informática""Química""Historia de España""Matemáticas""Artes""Psicología""Artes""Física"]
+            nombre="UHU", ciudad="Huelva", tipo="Pública", grados=["Ingeniería Mecánica""Farmacia""Biología""Lengua""Filosofía""Ingeniería Informática"
+            "Química""Historia de España""Matemáticas""Artes""Psicología""Artes""Física"]
         )
