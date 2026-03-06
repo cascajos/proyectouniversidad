f96ee024254edf31f3cf49168a6f2dfdccb2e649
arenita17 <hernandezbonalariadna@gmail.com>
2026-03-06 09:47:54 +0100

    ariadna

--- No Probado/ariadna/metodos_sistema_de_admision.py
+++ No Probado/ariadna/metodos_sistema_de_admision.py
@@ -1,3 +1,11 @@
+def procesar_admisiones (self):
+    for solicitud in self.solicitudes:
+        for grado, prioridad, nota in solicitud.preferencias:
+            if grado.esta_disponible():
+                grado.agregar_admitido(solicitud.estudiante)
+                solicitud.gradi_asignado = grado.nombre
+                solicitud.estado ="asignada"
+                break
 def generar_ranking(self,grado):
     ranking = sorted(
         grado.admitidos + grado.lista_espera,
@@ -11,3 +19,5 @@ def generar_estadistica(self):
     print (f"Solicitudes totales: {total}")
     print(f"solicitudes asignadas:{asignadas}")
     print(f"Tasa de éxito: {asignadas/total*100:.2f}%")
+def simular_escenarios (self):
+    print("Simulacion de escenarios no implementada aún.")
