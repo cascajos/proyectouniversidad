12ae3ca262969620ab3a3cbf2e0b8e798b3acd68
Pauu87 <hermipa22@gmail.com>
2026-03-16 11:19:17 +0100

    p

--- No Probado/atributos_universidad.py
+++ No Probado/atributos_universidad.py
@@ -1,10 +1,10 @@
 class Grados:
-    def __init__(self,nombre,universidad,nota_de_corte,ponderaciones,plazas,alejandro):
+    def __init__(self,nombre,universidad,nota_de_corte,ponderaciones,plazas):
         self.nombre=nombre
         self.universidad=universidad
         self.nota_corte=nota_de_corte
         self.ponderaciones=ponderaciones
         self.plazas=plazas
-        self.alejandro=alejandro
+
         
 
