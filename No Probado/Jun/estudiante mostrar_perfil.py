16519a7fe0f5abfcb17f4d312c5d3606eb1ee763
Junfangchong <jianj.fan@educa.jcyl.es>
2026-03-16 11:07:53 +0100

    jun

--- No Probado/Jun/estudiante mostrar_perfil.py
+++ No Probado/Jun/estudiante mostrar_perfil.py
@@ -3,6 +3,6 @@ def mostrar_perfil(self):
         print(f"Nombre: {self.nombre}")
         print(f"DNI: {self.DNI}")
         print("Notas EBAU:")
-        for asignatura,notas in self.notas_evau.items():
+        for asignatura,notas in self.notas_evau.items():#Enseña todas las notas de la evau
             print(f"  - {asignatura}: {notas}")
         print(f"Nota Bachillerato: {self.nota_bachillerato}")
