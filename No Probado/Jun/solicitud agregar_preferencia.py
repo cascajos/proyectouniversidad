0c06fb4ddb22faecc0f67b72b7319b0eb1747ba3
Junfangchong <jianj.fan@educa.jcyl.es>
2026-03-16 11:10:11 +0100

    jun

--- No Probado/Jun/solicitud agregar_preferencia.py
+++ No Probado/Jun/solicitud agregar_preferencia.py
@@ -1,4 +1,4 @@
 def agregar_preferencia(self,grado,prioridad):
-        nota=self.estudiante.calcular_nota_admision(grado)
-        lista=[grado,nota,prioridad]
-        self.preferencias.append(lista)
+        nota=self.estudiante.calcular_nota_admision(grado)#Calculo la nota de admisión para el grado
+        lista=[grado,nota,prioridad]#Creo la lista
+        self.preferencias.append(lista)#Lo añado a la lista de preferencias
