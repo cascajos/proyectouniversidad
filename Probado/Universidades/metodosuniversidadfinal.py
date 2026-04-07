dc306741998bffe7f4c9caf460c892d8945a2614
arenita17 <hernandezbonalariadna@gmail.com>
2026-04-07 13:21:25 +0200

    ari

--- Probado/Universidades/metodosuniversidadfinal.py
+++ Probado/Universidades/metodosuniversidadfinal.py
@@ -12,30 +12,28 @@ class Universidad:
 
 def añadir_grado():
 # Pedimos el grado al usuario y lo devolvemos
-        return input("Introduce el grado que desees buscar, sin errores ortográficos: ").strip()
+    return input("Introduce el grado que desees buscar, sin errores ortográficos: ").strip()
 
 def buscar_grado(lista_universidades, grado_buscado):
     ciudades_encontradas = []
 
     for uni in lista_universidades:
         if grado_buscado in uni.grados:
-            ciudades_encontradas.append(uni.ciudad)
-
+            ciudades_encontradas.append(f"{uni.nombre} ({uni.ciudad})")
     if ciudades_encontradas:
-        print(f"Tu grado ({grado_buscado}) está disponible en: {', '.join(ciudades_encontradas)}")
+            print(f"Tu grado ({grado_buscado}) está disponible en: {', '.join(ciudades_encontradas)}")
     else:
-        print(f"Lo sentimos, el grado '{grado_buscado}' no está disponible en nuestras universidades.")
-
+            print(f"Lo sentimos, el grado '{grado_buscado}' no está disponible en nuestras universidades.")
 def main():
     universidad1 = Universidad("UBU", "Burgos", "Pública", ["Ingeniería Informática","Derecho","Medicina","Criminología","Farmacéutica","Ingeniería eléctrica", "Ingeniería robótica","Física","Matemáticas","Artes","Historia de España","Lengua Castellana","Filosofía"])
     universidad2 = Universidad("UPM", "Madrid", "Pública", ["Astronomía","Enfermería","Criminología","Derecho","Geología","Ingeniería Informática","Aviación", "Arquitectura","Historia de España","Filosofía","Medicina","Robótica"])
-    universidad3 = Universidad("EHU", "Bilbao", "Pública", ["Derecho","Farmacia","Ingeniería Electrónica","Mecánica","Matemáticas","Arte","Biología", "Ingeniería Informática","Criminología","Economía"])
-    universidad4 = Universidad("UPSAL", "Salamanca", "Privada", ["Física","Medicina","Ingeniería Informática","Matemáticas","Derecho","Robótica", "Geología","Filosofía","Criminología","Aviación"])
+    universidad3 = Universidad("UPV", "Bilbao", "Pública", ["Derecho","Farmacia","Ingeniería Electrónica","Mecánica","Matemáticas","Arte","Biología", "Ingeniería Informática","Criminología","Economía"])
+    universidad4 = Universidad("USAL", "Salamanca", "Pública", ["Física","Medicina","Ingeniería Informática","Matemáticas","Derecho","Robótica", "Geología","Filosofía","Criminología","Aviación"])
     universidad5 = Universidad("UHU", "Huelva", "Pública", ["Ingeniería Mecánica","Farmacia","Biología","Lengua","Filosofía","Ingeniería Informática", "Química","Historia de España","Matemáticas","Artes","Psicología","Física"])
 
     lista_universidades = [universidad1, universidad2, universidad3, universidad4, universidad5]
 
-    # Mostramos todos los datos
+# Mostramos todos los datos
     for uni in lista_universidades:
         print(uni.mostrar_ofertas())
 
