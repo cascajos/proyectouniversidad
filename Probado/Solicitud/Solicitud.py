3774f26ab050eb04f6f7931e0588c353efb7eb6a
cascajos <cascajogarcinunojorge@gmail.com>
2026-04-09 10:12:18 +0200

    jorge

--- Probado/Solicitud/Solicitud.py
+++ Probado/Solicitud/Solicitud.py
@@ -12,7 +12,7 @@ class Solicitud:
         self.preferencias.append(lista)
 
     def ordenar_preferencias(self):
-        self.preferencias.sort(key=lambda x:x[2])#Chat GPT (esto basicamente ordena las preferencias)
+        self.preferencias.sort(key=lambda x:x[2]) #(esto basicamente ordena las preferencias)
         print("--------Preferencias por orden--------")
         for i in range(0,len(self.preferencias)):#imprime las preferencias por orden
             print(f"Prioridad: {self.preferencias[i][1]}, Grado: {(self.preferencias[i][0]).nombre}, Nota de admisión: {self.preferencias[i][2]}")
